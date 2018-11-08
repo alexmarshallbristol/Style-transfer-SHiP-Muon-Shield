438407f2b202699c3225ef5783155c4c5ccafcb1
Alex Marshall <alex.marshall@bristol.ac.uk>
2018-11-08 11:05:27 +0100

    pre-process ideas

---
+++ pre_process.py
@@ -0,0 +1,15 @@
+import numpy
+
+'''
+Need to extract muons from veto plane in fairship
+
+Then pre process here:
+
+	Define absolute maximum edges:
+		x: -1000 to 1000
+		y: -1000 to 1000
+		px: -20 to 20
+		py: -20 to 20
+		pz: 0 to 400
+
+'''
